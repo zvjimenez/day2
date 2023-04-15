68af952258ea0214f30941621a95a2166e0cc7f8
feb27 <feb27@10.0.0.123>
2023-04-15 10:34:40 -0400

    implemented main() & add()

---
+++ src/GitDay2/DemoDay2.java
@@ -0,0 +1,12 @@
+package GitDay2;
+
+public class DemoDay2 {
+
+	public static void main (String args []) {
+		System.out.println("Pushing to Remote Repository..");
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+}
